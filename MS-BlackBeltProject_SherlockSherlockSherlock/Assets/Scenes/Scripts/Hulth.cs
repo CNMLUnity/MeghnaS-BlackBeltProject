ea9aa8f3db683836189afd9c8017d88a80098f6e
CNMLUnity <133922391+CNMLUnity@users.noreply.github.com>
2025-05-08 17:55:33 -0600

    zxc

---
+++ MS-BlackBeltProject_SherlockSherlockSherlock/Assets/Scenes/Scripts/Hulth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hulth : MonoBehaviour
+{
+    public GameObject [] hearts;
+    public int lives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
