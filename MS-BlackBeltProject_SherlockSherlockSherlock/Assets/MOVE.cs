e99b4532b16943d4b41f6dda3730db2d08775742
CNMLUnity <133922391+CNMLUnity@users.noreply.github.com>
2025-05-15 17:56:02 -0600

    eeeeeeeeeeeeeeeee

---
+++ MS-BlackBeltProject_SherlockSherlockSherlock/Assets/MOVE.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOVE : MonoBehaviour
+{
+    public float Speed = 15f;
+    public bool running = false;
+    public bool canMove = true;
+    public Vector3 move;
+    public float _rotationSpeed = 50f;
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
+    void Update()
+    {
+       
+        if (canMove)
+        {
+
+            float vertical = Input.GetAxis("Vertical");
+            float horizontal = Input.GetAxis("Horizontal");            
+            Vector3 rotation = new Vector3(0, horizontal * _rotationSpeed * Time.deltaTime, 0);
+            move = transform.forward * Speed * Time.deltaTime * vertical;
+            transform.Rotate(rotation);
+            transform.position += move;
+        }
+        
+    }
+}
+
