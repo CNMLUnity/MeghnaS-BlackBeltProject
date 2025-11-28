42f243fa598dd488cb155748c09f98a9b228d5bc
CNMLUnity <cnmlunitybackup@gmail.com>
2025-11-28 16:53:07 -0700

    HOOOOOOLY CRAP
    We just spent nearly the ENTIRe TIME trying to fix a crapping pair of handcuffs and we needed to deletre ONE PHRASE HOOOLY CRAAAAAP

--- MS-BlackBeltProject_SherlockSherlockSherlock/Assets/Scenes/MY Scripts/Arrest.cs
+++ MS-BlackBeltProject_SherlockSherlockSherlock/Assets/Scenes/MY Scripts/Arrest.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class Arrest : MonoBehaviour
 {
     public Transform target;
-    public float speed = 5f;
+    public float speed = 1f;
     public GameObject handcuffs;
-    public GameObject Player;
+    public Transform Player;
+    //public UnityEngine.Vector3 handOffset = new UnityEngine.Vector3(0f, 0f, 0f);
     // Start is called before the first frame update
     void Start()
     {
+        //handcuffs.transform.SetParent(Player);
         handcuffs.SetActive(false);
         Debug.Log(handcuffs.activeInHierarchy);
-        Debug.Log("The player's position is" + Player.transform.position + "The handcuffs position is" +  handcuffs.transform.position);
+        //Debug.Log("The player's position is" + Player.transform.position + "The handcuffs position is" +  handcuffs.transform.position);
     }
 
     // Update is called once per frame
@@ -21,12 +24,15 @@ public class Arrest : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.T))
         {
-            handcuffs.transform.position = Player.transform.position;
+            //handcuffs.transform.position = Player.transform.position;
             handcuffs.SetActive(true);
+            //handcuffs.transform.localPosition = new Vector3(0f,1.2f,0f);
         }
         if (handcuffs.activeSelf)
         {
-            transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+            handcuffs.transform.position =  UnityEngine.Vector3.Lerp(handcuffs.transform.position, Player.transform.position, speed * Time.deltaTime);
+            //transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+            Debug.Log("The player's position is" + Player.transform.position + "The handcuffs position is" +  handcuffs.transform.position);
         }
     }
 }
