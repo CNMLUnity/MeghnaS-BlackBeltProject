68da0ea8e45fefcb238455f97ebba4e0c1c0d771
CNMLUnity <cnmlunitybackup@gmail.com>
2025-12-04 17:15:35 -0700

    Update Bobby Arrest.cs

--- MS-BlackBeltProject_SherlockSherlockSherlock/Assets/Scenes/MY Scripts/Bobby Arrest.cs
+++ MS-BlackBeltProject_SherlockSherlockSherlock/Assets/Scenes/MY Scripts/Bobby Arrest.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class BobbyArrest : MonoBehaviour
 {
+    public Arrest arrest;
+    public bool hasHandcuffs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasHandcuffs = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(arrest.handcuffs.SetActive(true))
+        {
+            hasHandcuffs = true;
+        }
+    }
+    void OnCollisionEnter (Collision collision)
+    {
+        if(collision.gameObject.tag == "Player" && hasHandcuffs == true)
+        {
+             SceneManager.LoadScene(8);
+        }
     }
 }
