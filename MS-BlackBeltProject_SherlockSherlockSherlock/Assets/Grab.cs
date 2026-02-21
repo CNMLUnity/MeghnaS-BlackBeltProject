cc47ce5f45c0ed644bde52f07c5159a4f6cc7678
CNMLUnity <cnmlunitybackup@gmail.com>
2026-02-20 18:04:35 -0700

    My life is in a deadlock
    PLEASE ACTUALLY COMMIT

--- MS-BlackBeltProject_SherlockSherlockSherlock/Assets/Grab.cs
+++ MS-BlackBeltProject_SherlockSherlockSherlock/Assets/Grab.cs
@@ -10,6 +10,7 @@ public class Grab : MonoBehaviour
     public GameObject SilverSample;
     public GameObject SilverSmall;
     public GameObject PlayerHand;
+    public bool isTouchingSilver;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,20 +19,30 @@ public class Grab : MonoBehaviour
         SilverPrecipipitate.SetActive (false);
         AquaRegia.SetActive (true);
         SilverSmall.SetActive(false);
+        isTouchingSilver = true;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {        
+        if(isTouchingSilver = true && Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.LogError("You have pressed G.");
+            SilverSmall.SetActive (true);
+            SilverSmall.transform.position = PlayerHand.transform.position;
+            isTouchingSilver = true;
+        }
+        /* if(other.gameObject.name == "Circle" && Input.GetKeyDown(KeyCode.R))
+        {
+            SilverSmall.SetActive(false);
+            AquaRegia.SetActive(false);
+            GoldPrecipitate.SetActive(true);
+            SilverPrecipipitate.SetActive(true);
+        } */
     }
     void OnTriggerEnter (Collider other)
     {
         Debug.LogWarning(other.gameObject.name);
-        if(other.gameObject.name == SILVER && Input.GetKeyDown("G"))
-        {
-            SilverSmall.SetActive (true);
-            SilverSmall.transform.position = PlayerHand.transform.position;
-        }
+
     }
 }
