a6a3ecce4da4fb5799b276ab145cbbb3d882595e
CNMLUnity <cnmlunitybackup@gmail.com>
2026-02-27 17:50:13 -0700

    sdad

--- MS-BlackBeltProject_SherlockSherlockSherlock/Assets/Grab.cs
+++ MS-BlackBeltProject_SherlockSherlockSherlock/Assets/Grab.cs
@@ -11,6 +11,8 @@ public class Grab : MonoBehaviour
     public GameObject SilverSmall;
     public GameObject PlayerHand;
     public bool isTouchingSilver;
+    public bool isTouchingCircle;
+    public float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,30 +21,37 @@ public class Grab : MonoBehaviour
         SilverPrecipipitate.SetActive (false);
         AquaRegia.SetActive (true);
         SilverSmall.SetActive(false);
-        isTouchingSilver = true;
+        isTouchingSilver = false;
+        isTouchingCircle = false;
     }
-
+    void OnTriggerEnter (Collider other)
+    {
+        Debug.LogWarning(other.gameObject.name);
+        if(other.gameObject.name == "Circle")
+        {
+            isTouchingCircle = true;
+        }
+    }
     // Update is called once per frame
     void Update()
     {        
         if(isTouchingSilver = true && Input.GetKeyDown(KeyCode.G))
         {
             Debug.LogError("You have pressed G.");
-            SilverSmall.SetActive (true);
-            SilverSmall.transform.position = PlayerHand.transform.position;
-            isTouchingSilver = true;
+            SilverSmall.SetActive(true);
+            //isTouchingSilver = true;
+            
         }
-        /* if(other.gameObject.name == "Circle" && Input.GetKeyDown(KeyCode.R))
+        if (SilverSmall.activeInHierarchy)
+        {
+            SilverSmall.transform.position = PlayerHand.transform.position; //UnityEngine.Vector3.Lerp(SilverSmall.transform.position, PlayerHand.transform.position, speed * Time.deltaTime);;
+        }
+        if(isTouchingCircle = true && Input.GetKeyDown(KeyCode.R))
         {
             SilverSmall.SetActive(false);
             AquaRegia.SetActive(false);
             GoldPrecipitate.SetActive(true);
             SilverPrecipipitate.SetActive(true);
-        } */
-    }
-    void OnTriggerEnter (Collider other)
-    {
-        Debug.LogWarning(other.gameObject.name);
-
+        }
     }
 }
