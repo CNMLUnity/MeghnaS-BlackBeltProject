ba42e9a5ebcfed35b8936b015a07a76571d383b6
CNMLUnity <cnmlunitybackup@gmail.com>
2026-05-22 17:49:31 -0600

    COMMIT IT, CRAP YOU
    Why am I not  locking in?

--- MS-BlackBeltProject_SherlockSherlockSherlock/Assets/MY Scripts/ShowMessage.cs
+++ MS-BlackBeltProject_SherlockSherlockSherlock/Assets/MY Scripts/ShowMessage.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShowMessage : MonoBehaviour
 {
+    public int currentScene;
     public GameObject ForgetfulBlock;
     public GameObject ForgetfulText;
     public GameObject MurderBlock;
@@ -21,5 +23,37 @@ public class ShowMessage : MonoBehaviour
         SpitefulText.SetActive(false);
         BobbyBlock.SetActive(false);
         BobbyText.SetActive(false);
+    }
+    void Show(Collider other)
+    {
+        if(other.gameObject.name == "Murderess" && Input.GetKeyDown(KeyCode.P))
+        {
+            MurderBlock.SetActive(true);
+            MurderText.SetActive(true);
+        }
+        if(other.gameObject.name == "Forgetful" && Input.GetKeyDown(KeyCode.P))
+        {
+            ForgetfulBlock.SetActive(true);
+            ForgetfulText.SetActive(true);
+        }
+        if(other.gameObject.name == "Spiteful" && Input.GetKeyDown(KeyCode.P))
+        {
+            SpitefulBlock.SetActive(true);
+            SpitefulText.SetActive(true);
+        }
+        if(other.gameObject.name == "Bobby" && Input.GetKeyDown(KeyCode.P))
+        {
+             BobbyBlock.SetActive(true);
+            BobbyText.SetActive(true);
+        }
+    }
+    void OnTriggerEnter (Collider other)
+    {
+        Show(other);
+        print(other.gameObject.name);
+        if(other.gameObject.name == "Murderess" && Input.GetKeyDown(KeyCode.F))
+        {
+            SceneManager.LoadScene(currentScene + 1);
+        }
     }
 }
